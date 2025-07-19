e6f869a0fd07bbafb45d7e058e36f5c5d01ed409
webdevgf1 <a4519262@gmail.com>
2025-07-19 20:09:45 +0100

    Update profile.js

--- api/profile.js
+++ api/profile.js
@@ -151,7 +151,7 @@ function generateDataDrivenConspiracy(profile) {
 }
 
 // Main serverless function
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // Handle CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
